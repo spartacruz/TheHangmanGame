18ec9479948a4f8a4e25400d3ad0e953e3dee96e
Yuri <yuri.barru@gmail.com>
2018-01-09 17:04:13 +0700

    Trying to converts secret word (word that user must have to guess) into "_" (underscore) for each letter, rather than shows the secret word literally.
    2 method added:
    
    1. scrambledWord that generate the secret word.
    2. arrayBuilder that helps concatenate every each letter ("_" underscore)

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,11 @@
+
 public class Game {
     public static void main(String [] args) {
         WordSupply word = new WordSupply();
-        System.out.println("You are guessing (" + word.countWord() + " Letter): "+ word.getTheWord());
+
+        word.scrambleWord();
+        System.out.println("You are guessing (" + word.countWord() + " Letter): " + word.getTheWord() +
+                " " + word.arrayBuild());
 
         System.out.println("You have guessed (0) wrong letters.");
         System.out.println("Guess a letter: ");
--- src/WordSupply.java
+++ src/WordSupply.java
@@ -1,13 +1,29 @@
 public class WordSupply {
     private String theWord;
+    private String[] scramble = new String[countWord()];
+
+    public int countWord() {
+        getTheWord();
+        return theWord.length();
+    }
+
     public String getTheWord() {
         theWord = "Inception";
         return theWord;
     }
 
-    public int countWord() {
-        getTheWord();
-        return theWord.length();
+    public void scrambleWord() {
+        for (int i = 0; i < countWord(); i++) {
+            scramble[i] = "_";
+        }
+    }
+
+    public String arrayBuild() {
+        StringBuilder builder = new StringBuilder();
+        for (String value : scramble) {
+            builder.append(value);
+        }
+        return builder.toString();
     }
 
 }
