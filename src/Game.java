5a315242b69b4640ef52c7570080517513df0db8
Yuri <yuri.barru@gmail.com>
2018-01-09 19:17:15 +0700

    Create a new void method called "guessProgress" that print current status which letter that match with user guess, where they are, and how many wrong letter that user guess

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 
 public class Game {
+
+    public static void guessProgress(int countWord, String getTheWord, String arrayBuilder, int attempt) {
+        System.out.println("You are guessing (" + countWord + " Letter): " + getTheWord +
+                " " + arrayBuilder);
+
+        System.out.println("You have guessed ("+ attempt +") wrong letters.");
+        System.out.println("Guess a letter: ");
+    }
+
     public static void main(String [] args) {
         //Create new object called word with 'WordSupply' as the type
         WordSupply word = new WordSupply();
@@ -9,11 +18,7 @@ public class Game {
 
         //Call scrambleWord() method at word Object
         word.scrambleWord();
-        System.out.println("You are guessing (" + word.countWord() + " Letter): " + word.getTheWord() +
-                " " + word.arrayBuild());
-
-        System.out.println("You have guessed (0) wrong letters.");
-        System.out.println("Guess a letter: ");
+        guessProgress(word.countWord(), word.getTheWord(), word.arrayBuild(), attempt);
 
         Scanner scanner = new Scanner(System.in);
 
