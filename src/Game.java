750544a21a90039e875edf8e33e1d77b77e487dc
Yuri <yuri.barru@gmail.com>
2018-01-09 18:13:10 +0700

    Experiment : Try to use method indexOf(String str) to finding out the index of the specified character or substring in a particular String (theWord). Returns the index of string str in a particular String.
    If found, system will print "we found that character! at index ...."
    
    if not, system will print "cannot found that character!"

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 
 public class Game {
     public static void main(String [] args) {
         //Create new object called word with 'WordSupply' as the type
         WordSupply word = new WordSupply();
+        int attempt = 10;
+        String theWord = word.getTheWord();
 
         //Call scrambleWord() method at word Object
         word.scrambleWord();
@@ -11,5 +14,21 @@ public class Game {
 
         System.out.println("You have guessed (0) wrong letters.");
         System.out.println("Guess a letter: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (attempt != 0) {
+            String input = scanner.next();
+
+            int foundAt = theWord.indexOf(input);
+            if (foundAt != -1) {
+                System.out.println("we found that character! at index " + foundAt);
+                break;
+            } else {
+                System.out.println("cannot found that character!");
+                break;
+            }
+        }
+
     }
 }
