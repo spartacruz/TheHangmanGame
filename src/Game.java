9aff870b1dd0ec3e883e51a67923eb8f17aeb512
Yuri <yuri.barru@gmail.com>
2018-01-09 14:05:23 +0700

    Re structure the question. Now, user can see how many letter they have to guess.

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,7 @@
 public class Game {
     public static void main(String [] args) {
         WordSupply word = new WordSupply();
-        System.out.println("You are guessing: " + word.getTheWord());
+        System.out.println("You are guessing (" + word.countWord() + " Letter): "+ word.getTheWord());
 
         System.out.println("You have guessed (0) wrong letters.");
         System.out.println("Guess a letter: ");
